be0a0476b9dc6cc489788810c051f3c6755f5ea5
yogi199811 <yogeshkushwaha1998@gmail.com>
2026-04-03 17:21:01 +0530

    add login

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -5,32 +5,47 @@ import bcryptjs from "bcryptjs";
 import sendEmail from "@/helpers/mailer";
 import { error } from "console";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 connectDB();
 
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
 
-export async function POST(request:NextRequest) {
-    try {
-        const reqBody =  await request.json()
+    const { email, password } = reqBody;
 
-        const {email,password} =  reqBody
-
-        const user = await User.findOne({email}) 
-        if(!user){
-            return NextResponse.json({error:"User does not exist with this email"},{status:400})
-        }
-
-     const isPasswordValid =   await bcrypt.compare(password,user.password)
-
-     if(!isPasswordValid){
-        return NextResponse.json({error:"Invalid Password"},{status:400})
-     }
-
-
-        
-    } catch (error:any) {
-        return NextResponse.json({error:error.message},{status:500})
-        
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "User does not exist with this email" },
+        { status: 400 },
+      );
     }
-    
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return NextResponse.json({ error: "Invalid Password" }, { status: 400 });
+    }
+
+    const token = await jwt.sign(
+      { id: user._id, username: user.username, email: user.email },
+      process.env.TOKEN_SECRET!,
+      { expiresIn: "10d" },
+    );
+
+   const response =  NextResponse.json(
+      { message: "Login Successfull", success: true },
+      { status: 200 },
+    );
+
+    response.cookies.set("token",token,{
+        httpOnly:true
+    })
+
+    return response
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
